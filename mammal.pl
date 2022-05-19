4f38edcf8788c8dee6d584e5861d9eb4d84ab9d5
Tomás Ribeiro <81585620+up202004953@users.noreply.github.com>
2022-05-19 15:15:00 +0100

    Add files via upload

--- mammal.pl
+++ mammal.pl
@@ -1,6 +1,3 @@
-:- dynamic yes/1.
-yes(yes).
-
 :- dynamic animal/1.
 
 animal("Morcego").
@@ -10,25 +7,76 @@ animal("Golfinho").
 
 :- dynamic node/4.
 node(1, "Terrestre", 2, 3).
-node(2,"Doméstico", 4, 5).
+node(2,"Bigodes", "Gato", "Porco").
 node(3, "Voa", "Morcego", "Golfinho").
-node(4, "Estimação", "Gato", "Porco").
-node(5, "Quinta", "Porco", "Gato").
+
+:- dynamic yes/1.
+yes("Sim").
+
+:- dynamic input/1.
+input(Out) :- read(In),
+    		  atom_length(In,L1), L is L1-1, 
+    		  sub_atom(In, 0, 1, _, First),
+    		  sub_atom(In, 1, L, _, Last),
+    		  string_upper(First, Upper),
+    		  atom_concat(Upper, Last, Atom),
+    	      atom_string(Atom, Out).
 
 :- dynamic start/0.
-start :- ask(1).
+start :- write("Escreva sempre em minúscula com acentuação"), nl,
+         ask(1).
 
-:- dynamic ask/1.
+:- dynamic ask/2.
+ask(Id) :- animal(Id),
+    	   end(Id).
+
 ask(Id) :- 
-    animal(Id) -> write(Id), !; true,
     node(Id,Q,_,_),
     write(Q),
-	read(Ans),
+	input(Ans),
     yes(Ans) -> node(Id,_,Y,_), ask(Y); node(Id,_,_,N), ask(N).
+
+:- dynamic end/1.
+end(Id) :- write(Id), nl,
+           write("É este animal?"),
+    	   input(Ans),
+    	   yes(Ans) -> write("Eu sabia!"); verify(Id).
+
+:- dynamic verify/1.
+verify(Id) :- write("Em que animal estavas a pensar?"),
+    		  input(Ans),
+    	      animal(Ans) -> path(Id, Ans); add(Ans).
+
+:- dynamic path/2.
+path(Id, Ans) :- write("--- "), write(Id), write(" ---"), nl,
+                 showPath(Id),
+				 write("--- "), write(Ans), write(" ---"), nl,
+                 showPath(Ans),
+                 write("Enganaste-te?"),
+                 input(Ans),
+                 yes(Ans) -> write("Eu sabia, eu estou sempre certo!"); add(Id).
+
+:- dynamic showPath/1.
+showPath(1).
+showPath(Id) :- node(BY,QY,Id,_) -> write(QY), write(" Sim"), nl, showPath(BY);
+    		    node(BN,QN,_,Id), write(QN), write(" Não"), nl, showPath(BN).
+
+:- dynamic add/1.
+add(Ans) :- write("Não o tenho: "), write(Ans).
 
 :- dynamic save/0.
 save :-
     tell('mammal.pl'),
     listing(animal),
+    listing(node),
+    listing(yes),
+    listing(input),
+    listing(start),
+    listing(ask),
+    listing(end),
+    listing(verify),
+    listing(path),
+    listing(showPath),
+    listing(add),
     listing(save),
     told.
