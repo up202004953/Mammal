53710be630defea31b75bd9269f37d4d6af844a3
Tomás Ribeiro <81585620+up202004953@users.noreply.github.com>
2022-05-26 12:44:39 +0100

    Insert database

--- mammal.pl
+++ mammal.pl
@@ -1,13 +1,268 @@
 :- dynamic animal/1.
-animal("Morcego").
-animal("Porco").
+animal("Ouriço-Cacheiro").
+animal("Furão").
+animal("Rato").
+animal("Esquilo").
+animal("Chinchilla").
+animal("Porquinho da India").
+animal("Hamster").
+animal("Cão").
+animal("Coelho").
 animal("Gato").
+animal("Cabra").
+animal("Carneiro").
+animal("Boi").
+animal("Mula").
+animal("Burro").
+animal("Ponei").
+animal("Egua").
+animal("Cavalo").
+animal("Coelho").
+animal("Ovelha").
+animal("Porco").
+animal("Cão").
+animal("Camelo").
+animal("Dromedário").
+animal("Lhama").
+animal("Alpaca").
+animal("Bufalo").
+animal("Raposa").
+animal("Corço").
+animal("Gamo").
+animal("Rena").
+animal("VeadO").
+animal("Antilocapra").
+animal("Veado").
+animal("Rinoceronte").
+animal("Gazela").
+animal("Impala").
+animal("Órix").
+animal("Palanca").
+animal("Cobo").
+animal("Muflão").
+animal("Bongo").
+animal("Bufalo").
+animal("Girafa").
+animal("Cabra").
+animal("Camurça").
+animal("Ocapi").
+animal("Gnu").
+animal("Bisonte").
+animal("Preguiça").
+animal("Gibão").
+animal("Coala").
+animal("Facocero").
+animal("Lebre").
+animal("Ouriço-Cacheiro").
+animal("Cão-da-Pradaria").
+animal("Marmota").
+animal("Capivara").
+animal("Wombat").
+animal("Elefante").
+animal("Camelo").
+animal("Dromedário").
+animal("Gorila").
+animal("Canguru").
+animal("Wallaby").
+animal("Zebra").
+animal("Panda").
+animal("Elefante").
+animal("Hipopótamo").
+animal("Lémur").
+animal("Babuíno").
+animal("Saguíno").
+animal("Macaco").
+animal("Orangotango").
+animal("Chimpanzé").
+animal("Jupará").
+animal("Humano").
+animal("Anta").
+animal("Urso").
+animal("Javali").
+animal("Panda-Vermelho").
+animal("Raposa").
+animal("Esquilo").
+animal("Glutão").
+animal("Guaxinim").
+animal("Gambá").
+animal("Leirão").
+animal("Lince").
+animal("Chita").
+animal("Jaguar").
+animal("Leopardo").
+animal("Leão").
+animal("Tigre").
+animal("Ocelote").
+animal("Dingo").
+animal("Lobo").
+animal("Coiote").
+animal("Mangusto").
+animal("Suricata").
+animal("Gineta").
+animal("Arminho").
+animal("Marta").
+animal("Doninha").
+animal("Hiena").
+animal("Fossa").
+animal("Tatu").
+animal("Diabo-da-tasmânia").
+animal("Texugo").
+animal("Toupeira").
+animal("Porco-formigueiro").
+animal("Papa-formigas").
+animal("Morcego").
+animal("Morsa").
+animal("Leão-Marinho").
+animal("Foca").
+animal("Narval").
+animal("Orca").
+animal("Beluga").
+animal("Orca").
+animal("Baleia").
 animal("Golfinho").
+animal("Toninha").
+animal("Elefante-Marinho").
+animal("Peixe-Boi").
+animal("Dugongo").
+animal("Castor").
+animal("Ratão-do-Banhado").
+animal("Hipopotamo").
+animal("Capivara").
+animal("Ornitorrinco").
+animal("Lontra").
 
 :- dynamic node/4.
-node(1, "Terrestre", 2, 3).
-node(2,"Bigodes", "Gato", "Porco").
-node(3, "Voa", "Morcego", "Golfinho").
+node(1, "É totalmente terrestre?", 2, 113).
+node(2, "É doméstico?", 3, 32).
+node(3, "É de estimação?", 4, 14).
+node(4, "É um roedor?", 5, 12).
+node(5, "Tem espinhos?", "Ouriço-Cacheiro", 6).
+node(6, "Tem um corpo longo?", "Furão", 7).
+node(7, "Tem uma cauda?", 8, 11).
+node(8, "Tem uma cauda peluda?", 9, "Rato").
+node(9, "Come bolotas?", "Esquilo", "Chinchilla").  
+node(10, "Tem a palavra India no nome?", "Porquinho da India", "Hamster").
+node(11, "Tem bigodes longos?", 13, "Cão").
+node(12, "Tem cauda curta?", "Coelho", "Gato").
+node(13, "É da quinta?", 14, 26).
+node(14, "Tem cornos?", 15, 18).
+node(15, "Tem barba?", "Cabra", 16).
+node(16, "Produz leite?", "Vaca", 17).
+node(17, "Dá lã?", "Carneiro", "Boi"). 
+node(18, "É usado para transporte de carga?", 19, 23).
+node(19, "Está relacionado com a falta de inteligência?", 20, 21).
+node(20, "É infertil?", "Mula", "Burro").
+node(21, "É de grande porte?", 22, "Ponei").
+node(22, "Produz Leite?", "Egua", "Cavalo").
+node(23, "É herbívoro?", 24, 25).
+node(24, "É de pequeno porte?", "Coelho", "Ovelha").
+node(25, "Tem uma cauda espiral?", "Porco", "Cão").
+node(26, "Vive no deserto?", 27, 28).
+node(27, "Tem mais que uma bossa?", "Camelo", "Dromedário").
+node(28, "Dá lã?", 29, 30).
+node(29, "Tem orelhas pontiagudas?", "Lhama", "Alpaca").
+node(30, "Tem cornos?", "Bufalo", "Raposa").
+node(31, "É herbívoro?", 31, 72).
+node(32, "Tem galhadas?", 33, 38).
+node(33, "As suas galhadas são pequenas?", "Corço", 34).
+node(34, "Tem cauda pequena?", 35, "Gamo").
+node(35, "Ambos os géneros tem galhadas?", "Rena", 36).
+node(36, "A sua pelagem é escura?", "Alce", 37).
+node(37, "Tem riscas brancas no pescoço?", "Antilocapra", "Veado").
+node(38, "Tem cornos?", 39, 53).
+node(39, "Tem chifres no nariz?", "Rinoceronte", 40).
+node(40, "Os seus cornos são grandes?", 41, 48).
+node(41, "Vive na savana?", 42, 46).
+node(42, "É conhecido pela sua velocidade?", 43, 44).
+node(43, 'É conhecido por fazer "Stotting"?', "Gazela", "Impala").
+node(44, "Os seus cornos são retos?", "Órix", 45).
+node(45, "Os seus cornos são curvos para trás?", "Palanca", "Cobo").
+node(46, "Os seus cornos são espirais?", "Muflão", 47).
+node(47, "Tem riscas no seu corpo?", "Bongo", "Bufalo").
+node(48, "Tem um pescoço longo?", "Girafa", 49).
+node(49, "Vive na montanha?", 50, 51).
+node(50, "Tem barba?", "Cabra", "Camurça").
+node(51, "Tem riscas no seu corpo?", 52, "Bisonte").
+node(52, "As suas riscas são brancas e pretas?", "Ocapi", "Gnu").
+node(53, "Tem menos de um metro de altura?", 54, 63).
+node(54, "Tem braços longos?", 55, 56).
+node(55, "Tem pouca mobilidade?", "Preguiça", "Gibão").
+node(56, "Está relacionado com as árvores?", "Coala", 57).
+node(57, "Tem presas?", "Facocero", 58).
+node(58, "Tem orelhas grandes?", "Lebre", 59).
+node(59, "É um roedor?", 60, "Wombat").
+node(60, "Tem cauda?", 61, "Capivara").
+node(61, "Tem espinhos?", ""Ouriço-Cacheiro", 62).
+node(62, "A sua cauda é curta?", "Cão-da-Pradaria", "Marmota").
+node(63, "Vive no deserto?", 64, 66).
+node(64, "Tem dentes de marfim?", "Elefante", 65).
+node(65, "Tem mais que uma bossa?", "Camelo", "Dromedário").
+node(66, "Anda sobre duas patas?", 67, 69).
+node(67, "Tem cauda?", 68, "Gorila").
+node(68, "Possui pernas alongadas?", "Canguru", "Wallaby").
+node(69, "Tem cor branca e preta?", 70, 71).
+node(70, "Tem cascos?", "Zebra", "Panda").
+node(71, "Tem tromba?", "Elefante", "Hipopótamo").
+node(72, "É omnívero?", 73, 90).
+node(73, "É um primata?", 74, 81).
+node(74, "Possui o corpo totalmente revestido por pêlos?", 75, 80).
+node(75, "Possui o focinho achatado?", 76, "Lémur").
+node(76, "Tem cauda?", 77, 79).
+node(77, "A cauda é curta?", "Babuíno", 78).
+node(78, "É de pequeno porte?", "Saguíno", "Macaco").
+node(79, "Os seus pêlos são alaranjados?", "Orangotango", "Chimpanzé").
+node(80, "Tem cauda?", "Jupará", "Humano").
+node(81, "Tem peso elevado?", 82, 84).
+node(82, "Tem pêlo?", 83, "Anta").
+node(83, "Possui garras?", "Urso", "Javali").
+node(84, "Possui orelhas pontiagudas?", 85, 86).
+node(85, "Os seus pêlos são vermelhos e pretos?", "Panda-Vermelho", "Raposa").
+node(86, "Possui a cauda arredondada?", 87, 88).
+node(87, "É de pequeno porte?", "Esquilo", "Glutão").
+node(88, "Possui a cauda listrada?", "Guaxinim", 89).
+node(89, "Possui garras?", "Gambá", "Leirão").
+node(90, "É um felino?", 91, 97).
+node(91, "O pêlo possui manchas circulares(pintas, manchas,...)?", 91, 95).
+node(92, "Possui orelhas pontiagudas?", "Lince", 93).
+node(93, "Possui linhas negras à volta do focinho?", "Chita", 94).
+node(94, "Tem rosettas(marcas em forma de flôr) com pintas no meio?", "Jaguar", "Leopardo").
+node(95, "Tem juba?", "Leão", 96).
+node(96, "É de grande porte?", "Tigre", "Ocelote").
+node(97, "É um canídeo?", 98, 100).
+node(98, "É geralmente alaranjado?", "Dingo", 99).
+node(99, "Tem orelhas redondas?", "Lobo", "Coiote").
+node(100, "Possui o corpo longo?", 101, 108).
+node(101, "É de pequeno porte?", 102, 107).
+node(102, "Possui listras horizontais nas costas?", 103, 104).
+node(103, "A sua cauda é enorme?", "Mangusto", "Suricata").
+node(104, "Tem a ponta da cauda preta?", 105, 106).
+node(105, "Possui a cauda listrada?", "Gineta", "Arminho").
+node(106, "Possui uma camada de pêlo extra no inverno?", "Marta", "Doninha").
+node(107, "Tem pêlo?", "Hiena", "Fossa").
+node(108, "É de pequeno porte?", 109, 112).
+node(109, "Tem orelhas?", 110, "Toupeira").
+node(110, "Possui armadura?", "Tatu", 111).
+node(111, "Os seus bigodes são grandes?", "Diabo-da-tasmânia", "Texugo").
+node(112, "As suas orelhas são grandes?", "Porco-formigueiro", "Papa-formigas").
+node(113, "Voa?", "Morcego", 114).
+node(114, "Possui barbatanas?", 115, 127).
+node(115, "Está relacionado com os Polos?", 116, 121).
+node(116, "Tem dentes grandes?", "Morsa", 117).
+node(117, "Tem orelhas?", "Leão-marinho", 118).
+node(118, "Consegue viver fora de água?", "Foca", 119).
+node(119, "Possui chifre?", "Narval", 120).
+node(120, "É preto?", "Orca", "Beluga").
+node(121, "Tem barbatana dorsal?", 122, 125).
+node(122, "É preto?", "Orca", 123).
+node(123, "É o maior animal da Terra?", "Baleia", 124).
+node(124, "O seu focinho é comprido?", "Golfinho", "Toninha").
+node(125, "Consegue viver fora de água?", "Elefante-Marinho", 126).
+node(126, "É herbívoro?", "Peixe-Boi", "Dugongo").
+node(127, "Está relacionado com a madeira?", "Castor", 128).
+node(128, "É herbívoro?", 129, 131).
+node(129, "Tem cauda longa?", "Ratão-do-Banhado", 130).
+node(130, "É de grande porte?", "Hipopotamo", "Capivara").
+node(131, "Poem ovos?", "Ornitorrinco", "Lontra").
 
 :- dynamic userPath/3. %Only stored while running
 
